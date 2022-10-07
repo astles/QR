64d750795005711243fc52ad5aa001e317e1dfb7
astles <66706743+astles@users.noreply.github.com>
2022-10-07 00:31:11 -0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.set_page_config(page_title="Streamlit projects", page_icon=None, layout="centered", initial_sidebar_state="expanded", menu_items=None)
+
+st.header("QR CODE MAKER")
+st.info("Make QRs for Devices, & Users")
+st.image("https://media.giphy.com/media/16dIgjWQjikY8/giphy.gif")
+
+col1, col2 = st.columns(2)
+
